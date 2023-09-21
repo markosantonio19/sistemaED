8105cffb040c51b242aa86e8ee0c86a6651c90ff
markosantonio19 <1433811@senati.pe>
2023-09-21 10:58:48 -0500

    agg mas datos para la bd

--- sistema de alumnos/sistema de alumnos/Cls_conexxion.cs
+++ sistema de alumnos/sistema de alumnos/Cls_conexxion.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Windows.Forms;
 using System.Security.Cryptography.X509Certificates;
+using System.Windows.Input;
 
 namespace sistema_de_alumnos
 {
@@ -15,7 +16,8 @@ namespace sistema_de_alumnos
         SqlDataAdapter reader;
         SqlConnection sqlcom;
 
-        public Cls_conexxion() {
+        public Cls_conexxion()
+        {
 
             sqlcom = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\ALUMNO\\Documents\\autenticar.mdf;Integrated Security=True;Connect Timeout=30");
         }
@@ -23,27 +25,54 @@ namespace sistema_de_alumnos
         public SqlConnection sql_conectar()
         {
 
-        try{
-            sqlcom.Open();   
-        }
-            catch { 
-        }
+            try
+            {
+                sqlcom.Open();
+            }
+            catch
+            {
+            }
             return sqlcom;
-    }
+        }
         public void sql_desconectar()
         {
             try
             {
                 sqlcom.Close();
-            } 
+            }
             catch { }
 
-            public bool validar(string user , string password)
+
+        }
+
+        public bool Validar(string user, string password)
+        {
+            bool valido = false;
+
+            // Usar parámetros en lugar de concatenación de cadenas para prevenir SQL Injection
+            string query = "SELECT nombre, clave FROM db.usuario WHERE nombre = @Usuario AND clave = @Clave";
+
+            using (SqlConnection connection = sql_conectar())
             {
+                connection.Open();
 
-                bool valido = false;    
-                string query =@"SELECT nombre, clave from db.usuario"
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    // Agregar parámetros para evitar SQL Injection
+                    command.Parameters.AddWithValue("@Usuario", user);
+                    command.Parameters.AddWithValue("@Clave", password);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            valido = true;
+                        }
+                    }
+                }
             }
-        }   
 
+            return valido;
+        }
+    }
 }
